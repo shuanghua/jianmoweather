02e020078c4e1783bca9138c2b4b5739f1faf38e
shuanghua <moshuanghua17@gmail.com>
2024-06-06 01:38:35 +0800

    Updated fix bug

--- data-android/network/src/test/java/dev/shuanghua/weather/data/android/network/RetrofitNetworkRequestTest.kt
+++ data-android/network/src/test/java/dev/shuanghua/weather/data/android/network/RetrofitNetworkRequestTest.kt
@@ -24,6 +24,14 @@ class RetrofitNetworkRequestTest {
 
 	@Before
 	fun setUp() {
+		val contentType = "application/json; charset=utf-8".toMediaType()
+		// 服务器会下发很多的字段, 我们只需要其中部分的字段, 所以忽略不使用的字段
+		// 服务器有时候会下发某个字段, 有时不会,所以我们需要用 explicitNulls = false 来禁止序列化 null 值
+		val format = Json {
+			ignoreUnknownKeys = true // 忽略服务器下发的多余字段
+			explicitNulls = false    // 不序列化 null 值
+		}
+
 		mockWebServer = MockWebServer()
 		apiService = Retrofit.Builder()
 			.baseUrl(
@@ -31,7 +39,7 @@ class RetrofitNetworkRequestTest {
 					"/fake/url/"
 				)
 			)
-			.addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+			.addConverterFactory(format.asConverterFactory(contentType))
 			.build()
 			.create(ApiService::class.java)
 	}
